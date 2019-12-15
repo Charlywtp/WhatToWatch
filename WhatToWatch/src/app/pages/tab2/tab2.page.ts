5026097c4ccd8944143b9acd34eb7ee6447b8830
Carlos Fernandez <charlywtp@gmail.com>
2019-12-15 12:59:52 +0100

    Added more groups on home page!

--- WhatToWatch/src/app/pages/tab2/tab2.page.ts
+++ WhatToWatch/src/app/pages/tab2/tab2.page.ts
@@ -9,30 +9,70 @@ import { Router } from '@angular/router';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+
   movieList: any;
-  aux: any[];
+  listingsList: any;
+  popularComedy: any;
+  popularAction: any;
+  popularAnimation: any;
+  popularHorror: any;
+  popularRomance: any;
+  popularMusic: any;
+  popularFiction: any;
+
 constructor(private movieService: MoviesService, private router: Router) {
 
   this.movieService.searchPopular().subscribe( (data: any) => {
   this.movieList = data.results;
-  console.log(this.movieList);
   });
+
+  this.movieService.searchListings().subscribe( (data: any) => {
+  this.listingsList = data.results;
+  });
+
+  this.movieService.searchPopularGenre(35).subscribe( (data: any) => {
+  this.popularComedy = data.results;
+  });
+
+  this.movieService.searchPopularGenre(28).subscribe( (data: any) => {
+  this.popularAction = data.results;
+  });
+
+  this.movieService.searchPopularGenre(16).subscribe( (data: any) => {
+  this.popularAnimation = data.results;
+  });
+
+  this.movieService.searchPopularGenre(27).subscribe( (data: any) => {
+  this.popularHorror = data.results;
+  });
+
+  this.movieService.searchPopularGenre(10749).subscribe( (data: any) => {
+  this.popularRomance = data.results;
+  });
+
+  this.movieService.searchPopularGenre(10402).subscribe( (data: any) => {
+    this.popularMusic = data.results;
+    });
+
+  this.movieService.searchPopularGenre(878).subscribe( (data: any) => {
+    this.popularFiction = data.results;
+    });
 
   }
 
-  searchChanged(search) {
+searchChanged(search) {
     if ( search === '') {
       this.movieService.searchPopular().subscribe( (data: any) => {
       this.movieList = data.results;
+      console.log(this.movieList);
       });
     } else {
     this.movieService.searchData(search).subscribe((data: any) => {
       this.movieList = data.results;
-      console.log(this.movieList);
     });
   }
 }
-  getDetails(id){
+getDetails(id){
       this.router.navigateByUrl(`/pages/tabs/tab2/movieDetails/${id}`);
   }
 }
--- WhatToWatch/src/app/services/movies.service.ts
+++ WhatToWatch/src/app/services/movies.service.ts
@@ -13,21 +13,32 @@ export class MoviesService {
 
   private apiKey: string = 'da958a49c54acd8b405ece0181cf2d8e';
 
-  private urlMovieDb: string = "https://api.themoviedb.org/3";
+  private urlMovieDb: string = 'https://api.themoviedb.org/3';
 
-  private populars: string = "/discover/movie?sort_by=popularity.desc";
+  private populars: string = '/discover/movie?sort_by=popularity.desc';
+
+  private listings: string = '/movie/now_playing?';
+
+  private popularComedy: string = '/discover/movie?api_key=';
 
   constructor(private http: HttpClient) { }
 
-  searchPopular(){
+  searchPopular() {
     let url =  ` ${this.urlMovieDb}${this.populars}&api_key=${this.apiKey}&language=es` ;
     return this.http.get(url);
   }
-
-  searchData(title: string){
+  searchListings() {
+    let url =  ` ${this.urlMovieDb}${this.listings}api_key=${this.apiKey}&language=es&page=1&region=ES` ;
+    return this.http.get(url);
+  }
+  searchData(title: string) {
     let url =  ` ${this.urlMovieDb}/search/movie?api_key=${this.apiKey}&query=${title}&sortb_by=popularity&language=es` ;
     return this.http.get(url);
 
+  }
+  searchPopularGenre(id){
+    let url =  ` ${this.urlMovieDb}${this.popularComedy}${this.apiKey}&lenguage=es&sortb_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${id}`;
+    return this.http.get(url);
   }
 
   getById(id: number) {
